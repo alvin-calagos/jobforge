d0ee17aae6a879fb509f199ea6f46a5fbcc106e7
alvinc <alvin.calagos@gmail.com>
2022-09-06 12:10:34 +0800

    Fixed auto complete address field

--- client/src/components/inputs/AddressAutoCompleteInput.js
+++ client/src/components/inputs/AddressAutoCompleteInput.js
@@ -1,63 +1,58 @@
-import React from "react"
+import React, { useState } from "react"
 import PlacesAutocomplete, { geocodeByAddress } from "react-places-autocomplete"
 
-class AddressAutoCompleteInput extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = { address: "" }
+const AddressAutoCompleteInput = ({ label, setOrgAddress }) => {
+  const [address, setAddress] = useState("")
+
+  const handleChange = (address) => {
+    setAddress(address)
   }
-  handleChange = (address) => {
-    this.setState({ address })
-  }
-  handleSelect = (address) => {
+  const handleSelect = (address) => {
     geocodeByAddress(address)
-      .then((results) => console.log(results))
+      .then((results) => setOrgAddress(results))
       .catch((error) => console.error("Error", error))
   }
-  render() {
-    return (
-      <PlacesAutocomplete
-        value={this.state.address}
-        onChange={this.handleChange}
-        onSelect={this.handleSelect}
-      >
-        {({ getInputProps, suggestions, getSuggestionItemProps, loading }) => (
-          <div>
-            <label>{this.props.label}</label>
-            <input
-              {...getInputProps({
-                placeholder: "Search Places ...",
-                className: "location-search-input"
-              })}
-            />
-            <div className="autocomplete-dropdown-container">
-              {loading && <div>Loading...</div>}
-              {suggestions.map((suggestion) => {
-                const className = suggestion.active
-                  ? "suggestion-item--active"
-                  : "suggestion-item"
-                // inline style for demonstration purpose
-                const style = suggestion.active
-                  ? { backgroundColor: "#fafafa", cursor: "pointer" }
-                  : { backgroundColor: "#ffffff", cursor: "pointer" }
-                return (
-                  <div
-                    {...getSuggestionItemProps(suggestion, {
-                      className,
-                      style
-                    })}
-                  >
-                    <span className="form-control">
-                      {suggestion.description}
-                    </span>
-                  </div>
-                )
-              })}
-            </div>
+  return (
+    <PlacesAutocomplete
+      value={address}
+      onChange={handleChange}
+      onSelect={handleSelect}
+    >
+      {({ getInputProps, suggestions, getSuggestionItemProps, loading }) => (
+        <div>
+          <label>{label}</label>
+          <input
+            {...getInputProps({
+              placeholder: "Search Places ...",
+              className: "location-search-input"
+            })}
+          />
+          <div className="autocomplete-dropdown-container">
+            {loading && <div>Loading...</div>}
+            {suggestions.map((suggestion) => {
+              const className = suggestion.active
+                ? "suggestion-item--active"
+                : "suggestion-item"
+              // inline style for demonstration purpose
+              const style = suggestion.active
+                ? { backgroundColor: "#fafafa", cursor: "pointer" }
+                : { backgroundColor: "#ffffff", cursor: "pointer" }
+              return (
+                <div
+                  {...getSuggestionItemProps(suggestion, {
+                    className,
+                    style
+                  })}
+                  key={suggestion.placeId}
+                >
+                  <span className="form-control">{suggestion.description}</span>
+                </div>
+              )
+            })}
           </div>
-        )}
-      </PlacesAutocomplete>
-    )
-  }
+        </div>
+      )}
+    </PlacesAutocomplete>
+  )
 }
 export default AddressAutoCompleteInput
--- client/src/pages/OrganisationNew.js
+++ client/src/pages/OrganisationNew.js
@@ -15,48 +15,49 @@ function OrganisationNew() {
   const [orgEmail, setOrgEmail] = useState("")
   console.log(orgAddress)
   return (
-    <>
-      <div>
-        <h2 className="text-center mt-5"> New Organisation </h2>
-        <div className="d-flex text-center organisation-form-wrapper">
-          <form className="d-flex mt-5">
-            <TextInput
-              placeholder="Name"
-              label="Organisation Name: "
-              type="text"
-              value={orgName}
-              onChange={(e) => setOrgName(e.target.value)}
-            />
-            <DateInput
-              label="Active Since: "
-              value={orgDateCreated}
-              onChange={(e) => setOrgDateCreated(e.target.value)}
-            />
-            <LocationSearchInput label="Address: " />
-            <TextInput
-              label="Selected Address"
-              value={orgAddress}
-              handleChange={getAddress}
-              onChange={(e) => setOrgAddress(e.target.value)}
-            />
-            <TextInput
-              placeholder="Phone"
-              label="Billing Phone: "
-              type="text"
-              value={orgPhone}
-              onChange={(e) => setOrgPhone(e.target.value)}
-            />
-            <TextInput
-              placeholder="Email"
-              label="Billing Email: "
-              type="text"
-              value={orgEmail}
-              onChange={(e) => setOrgEmail(e.target.value)}
-            />
-          </form>
-        </div>
+    <div>
+      <h2 className="text-center mt-5"> New Organisation </h2>
+      <div className="d-flex text-center organisation-form-wrapper">
+        <form className="d-flex mt-5">
+          <TextInput
+            placeholder="Name"
+            label="Organisation Name: "
+            type="text"
+            value={orgName}
+            onChange={(e) => setOrgName(e.target.value)}
+          />
+          <DateInput
+            label="Active Since: "
+            value={orgDateCreated}
+            onChange={(e) => setOrgDateCreated(e.target.value)}
+          />
+          <LocationSearchInput
+            label="Address: "
+            setOrgAddress={setOrgAddress}
+          />
+          <TextInput
+            label="Selected Address"
+            value={orgAddress}
+            handleChange={getAddress}
+            onChange={(e) => setOrgAddress(e.target.value)}
+          />
+          <TextInput
+            placeholder="Phone"
+            label="Billing Phone: "
+            type="text"
+            value={orgPhone}
+            onChange={(e) => setOrgPhone(e.target.value)}
+          />
+          <TextInput
+            placeholder="Email"
+            label="Billing Email: "
+            type="text"
+            value={orgEmail}
+            onChange={(e) => setOrgEmail(e.target.value)}
+          />
+        </form>
       </div>
-    </>
+    </div>
   )
 }
 
